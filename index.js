2343fe466d3ad4f75695433162a6afe5b662d74b
crccheck <c@crccheck.com>
2019-05-25 13:19:13 -0500

    draw a beginning control point

--- index.js
+++ index.js
@@ -18,35 +18,56 @@ class Designer {
     beg = [[20, -20], [50, 20]],
     end = [[20, 50], [70, 100]]
   ) {
+    this.$designerUI = $designerUI
     this.$designerSVG = $designerUI.find('svg')
     this.$targetSVG = $targetSVG
-    this.order = 1
     this.pitch = 2
     this.beg = beg
     this.end = end
     this.$designerSVG.attr('viewBox', `${-CROP} ${-BLEED} ${100 + 2 * CROP} ${100 + 2 * BLEED}`)
-    const matrix = this.$designerSVG[0].getScreenCTM().inverse()
+    this.matrix = this.$designerSVG[0].getScreenCTM().inverse()
+    this.setOrder(1)
 
     const self = this
     this.$designerSVG.on('click', function(evt, b) {
-      const x = evt.pageX * matrix.a + matrix.e
-      const y = evt.pageY * matrix.d + matrix.f
+      const x = evt.pageX * self.matrix.a + self.matrix.e
+      const y = evt.pageY * self.matrix.d + self.matrix.f
       const idx = evt.ctrlKey || evt.metaKey ? 1: 0
       if (evt.shiftKey) {
         self.end[idx] = [x, y]
       } else {
         self.beg[idx] = [x, y]
       }
+      self.setOrder(self.order) // DELETEME after html control points work
       self.draw()
     })
     $designerUI.find('input[type="radio"]').on('click', function () {
-      self.order = +this.value
+      self.setOrder(+this.value)
       self.draw()
     })
   }
 
   toString() {
     return [this.order, this.pitch, this.beg.slice(0, this.order), this.end.slice(0, this.order)].join('|')
+  }
+
+  setOrder(n/*: number */) {
+    this.order = n
+    this.$designerUI.find('.control').remove()
+    for (let i = 0; i < n; i++) {
+      const m2 = this.$designerSVG[0].getScreenCTM()
+      console.log(this.beg, m2)
+      const top = this.beg[i][1] * m2.a + m2.e
+      const left = this.beg[i][0] * m2.d + m2.f
+      $('<div class="control">+</div>')
+        .css({
+          top: `${top}px`,
+          left: `${left}px`
+        })
+        .appendTo(this.$designerUI)
+      // $('<div class="control">-</div>')
+      //   .appendTo(this.$designerUI)
+    }
   }
 
   getLinear(i) {
